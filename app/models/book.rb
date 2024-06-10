b3945a0bd39d0a8ab24ff6bf4cf046b5e77dbe36
wouterrodeyns <91271340+wouterrodeyns@users.noreply.github.com>
2024-06-10 23:06:23 +0200

    feat: implemented basic crud methods for book
    - Books can now be added, updated and deleted

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,7 @@
 class Book < ApplicationRecord
+  validates :title, presence: true
+  validates :author, presence: true
+  validates :publish_date, presence: true
+  validates :genre, presence: true
+  validates :isbn, presence: true, length: {is: 13}
 end
